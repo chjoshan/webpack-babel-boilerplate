b8dff1a1e6cd78b05f04bcc9c0bac107bb6ac2cc
Joshan Chaudhary <ch.joshan@gmail.com>
2018-09-22 13:50:10 +0200

    webpack babel boilerplate

---
+++ src/app.js
@@ -0,0 +1,2 @@
+console.log('Hello world!');
+module.hot.accept();
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        './src/app'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: './dist',
+        port: 3000,
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
